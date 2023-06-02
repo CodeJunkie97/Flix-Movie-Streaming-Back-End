8b8642791da173eb2bf8683f74e393e69f908cbc
CodeJunkie97 <ianmanners26@outlook.com>
2023-06-02 09:22:48 -0500

    Finishing Up the Base API. Will begin integrating with front end app

--- api/Index.js
+++ api/Index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
+const ListRoute = require("./routes/list");
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL).then(() => 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
+app.use("/api/lists", ListRoute);
 
 app.listen(8800, () => {
     console.log("Backend Server is running");
--- api/routes/movies.js
+++ api/routes/movies.js
@@ -46,7 +46,7 @@ router.delete("/:id", verify, async (req, res) => {
         res.status(500).json(err);
      }
     }else{
-        res.status(403).json("You can only update your account!")
+        res.status(403).json("You are not allowed!")
     }
 });
 
@@ -79,6 +79,20 @@ router.get("/random", verify, async (req, res) => {
      res.status(200).json(movie);
     }catch(err){
        res.status(500).json(err);
+    }
+});
+
+//GET ALL
+router.get("", verify, async (req, res) => {
+    if(req.user.isAdmin){
+     try{
+      const movies =  await Movie.find();
+        res.status(200).json(movies.reverse());
+     }catch(err){
+        res.status(500).json(err);
+     }
+    }else{
+        res.status(403).json("You are not allowed!")
     }
 });
 module.exports = router;
